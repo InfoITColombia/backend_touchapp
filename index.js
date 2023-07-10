433b0887d4c38673850916f9b34f85782c050212
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2023-07-09 21:10:07 -0500

    conexión a mysql e inserciín de datos

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 
 const express = require('express');
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors  = require("cors");
+
 
 const app = express ();
 
@@ -10,19 +11,33 @@ app.use(express.json());
 app.use(cors())
 
 const db = mysql.createConnection({
-    user : "root",
+    user : "touchapp",
     host : "localhost",
-    password : "password",
-    database : "logInSystem",
+    password : "touchapp",
+    database : "touchapp",
+    port:3306,
+    
 });
 
+//var db = mysql.createConnection('mysql://sa:g2a0g2a00407@localhost/touchapp?debug=true');
+
+db.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  
+    console.log('connected as id ' + db.database);
+  });
+
+
 app.post("/register", (req, res)=>{
-    const username = req.body.username;
-    const password = req.body.password;
+    const N_USUARIO = req.body.N_USUARIO;
+    const PWD_USUARIO = req.body.PWD_USUARIO;
 
     db.query(
-        "INSERT INTO users (username,password) VALUES (?,?)",
-        [username, password],
+        "INSERT INTO USUARIO (N_USUARIO,PWD_USUARIO) VALUES (?,?)",
+        [N_USUARIO, PWD_USUARIO],
         (err,result)=>{
             console.log(err);
         }
@@ -33,15 +48,17 @@ app.post('/login', (req,res)=>{
 
     const username = req.body.username;
     const password = req.body.password;
+    //console.log("REQUEST ES " & req)
 
     db.query(
-        "SELECT * FROM users WHERE username = ? and password = ?",
+        "SELECT * FROM USUARIO WHERE N_USUARIO = ? and PWD_USUARIO = ?",
         [username, password],
         (err,result)=>{
             if (err){
                 res.send({err:err})
+                //console.log(err)
             }else{
-                if (result){
+                if (result.length>0){
                     res.send(result)
                 }else{
                     res.send({message:"Wrong username/password!"})
@@ -51,6 +68,6 @@ app.post('/login', (req,res)=>{
     )
 });
 
-app.listen(3001, ()=>{
+app.listen(3002, ()=>{
     console.log("Servidor corriendo")
 })
